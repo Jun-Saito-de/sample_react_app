446944194125c8b3da2285b6929bdf9137404862
Apple <j.saito@design-engineering.co.jp>
2025-05-08 16:40:01 +0900

    2-3.map処理1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
+// リスト2-13 初期値が反映されない理由不明
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // App関数
 function App(props) {
-  // フィールドに入力したテキストを管理するためのもの
-  const [input, setInput] = useState("") 
-  // 表示するメッセージを管理するためのもの
-  const [message, setMessage] = useState("お名前は？")
-
-  // <input>のonChangeイベント用
-  // doInput フィールドの値が変更されると発生 
-  const doInput = (event) => {
-    setInput(event.target.value)
-  }
-  // <button>のonClickイベント用
-  // doClick ボタンをクリックした時に実行する処理  
-  const doClick = ()=> {
-    setMessage("こんにちは、" + input + "さん！")
-  }
-
-  // returnされるJSX内でフォームを用意
+  // nameとmailという値をもつオブジェクトの配列を用意。定数data
+  const data = [
+    {name:"Taro", mail:"taro@sato.com"},
+    {name:"Hana", mail:"hana@kato.com"},
+    {name:"Kosuke", mail:"kosuke@seto.com"},     
+  ]
   return (
-    <div className="App">
-      <h1 className={props.color}>{props.title}</h1>
-      <p className={`${props.color} clickable`}>{message}</p>
-      <div>
-        {/* イベント（onChange、onClick）に上述の関数を割り当て */}
-        <input type='text' onChange={doInput} />
-        <button onClick={doClick}>Click</button>
-      </div>
+    <div className='App'>
+      <h1>{props.title}</h1>
+      {/* 定数dataの内容をリスト表示。JavaScriptで記述 */}
+      <ul>
+        {/* data.mapでdataの値を繰り返し表示する */}
+        {/* 引数で渡されたitemには配列の各値が代入されている */}
+        {data.map((item,key)=> { 
+          return(
+          <li>{item.name} [{item.mail}]</li>
+        )
+        })}
+      </ul>
     </div>
   )
 }
