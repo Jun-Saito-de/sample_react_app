089152090167bb7509c2e40fc7a7c3e0da1b41c0
Apple <j.saito@design-engineering.co.jp>
2025-05-08 12:13:40 +0900

    react練習用初回のコミット

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// 必要なモジュール、ファイルなどを読み込むためのもの
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Reactに用意されている仮想DOMを扱うため用意されている
+// 引数に指定した「id=root」というエレメントをルートとするReactDOMオブジェクトを作成
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 仮想DOMの内容をレンダリングし、実際のDOMに反映
+// 「厳格モードでAppコンポーネントをレンダリングして表示する」作業
+root.render(
+  // この<React.〜>の部分がJSX
+  // React.StrictMode は「コードの実行をより厳格に制御するためのもの」
+  <React.StrictMode> 
+    <App />
+  </React.StrictMode>
+)
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
