b65237ed75b8805fb8cbf5ff11c42a63cadb34bb
JUN SAITO <jun@JUNnoMacBook-puro.local>
2025-05-11 19:55:22 +0900

    一時的にローカル変更を保存

--- src/App.js
+++ src/App.js
@@ -1,44 +1,81 @@
-// リスト2-13 初期値が反映されない理由不明
 import './App.css'
 import { useState, useEffect } from 'react'
 
-// App関数
 function App(props) {
-  var [count, setCount] = useState(0)
-  var [data,setData] = useState([
-    {
-      position: "absolute",
-      left:"0px", 
-      top: "0px",
-      width:"100%",height:"100%",
-      backgroundColor:"#fff0",
-    }
-  ])
 
-  const doClick = (event)=> {
-    const ob = {
-      position: "absolute",
-      left:(event.pageX - 50) + "px", 
-      top: (event.pageY - 50) + "px",
-      width:"100px",height:"100px",
-      backgroundColor:"#ff000066",
-      borderRadius:"50%",
-    }
-    data.push(ob)
-    setCount(count + 1)
+  const [input, setInput] = useState(1)
+  const [message, setMessage] = useState("");
+  const doInput = (event) =>{
+    setInput(event.target.value)
   }
+  const doClick = ()=> {
+    setMessage(`こんにちは、${input}さん！`);
+  }
+
+  useEffect(()=> {
+    if (input <= 0) {
+      setMessage("自然数を入力してください");
+      return
+    }
+
+    var prime = true
+    if (input == 1) {
+      prime = false
+    } else {
+      for (var i = 2; i <= input / 2; i++) {
+        if (input % i ===0) {
+          prime = false
+          break
+        }
+      }
+    } 
+    setMessage(prime ? "※素数です。" : "素数ではありません。")
+  }, [input]);
+  
 
   return (
     <div className='App'>
-      <h1>{props.title}</h1> 
-      <p>{count} objects.</p>
-      <div onClick={doClick}>
-        {data.map((item,key)=>{
-          return(<div style={item} key={key}></div>)
-        })}
-      </div>      
+      <h1 className={props.color}>{props.title}</h1>
+      <p className={`{props.color} "clickable"`}>
+        {message}
+      </p>
+      <div>
+        <input type='number' min="1" onChange={doInput} placeholder='自然数を入力してください'/>
+      </div>
     </div>
   )
 }
 
 export default App
+
+// import './App.css'
+
+
+// function Message(props) {
+//   return (
+//     <p className = "ClassA">{props.message}</p>
+//   )
+// }
+
+// function Data(props) {
+//   return (
+//     <ul>
+//       {props.data.map((ClipboardItem,key)=> {
+//         return(<li key={key}>{item.name}</li>)
+// })}
+//     </ul>
+//   )
+// }
+
+// function App(props){
+//   const data = [
+//     {name: "Taro"},
+//     {name: "Hanako"},
+//     {name: "Sachiko"},
+//   ]
+
+//   return (
+    
+//   )
+// }
+// export default App
--- src/index.js
+++ src/index.js
@@ -4,25 +4,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
 
-// Reactに用意されている仮想DOMを扱うため用意されている
-// 引数に指定した「id=root」というエレメントをルートとするReactDOMオブジェクトを作成
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// 仮想DOMの内容をレンダリングし、実際のDOMに反映
-// 「厳格モードでAppコンポーネントをレンダリングして表示する」作業
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  // この<React.〜>の部分がJSX
-  // React.StrictMode は「コードの実行をより厳格に制御するためのもの」
-  <React.StrictMode> 
-    <App title = "Hello,World!" message = "これはcolor=redの表示です。" color = "blue"/> 
-    {/* /* Appタグにtitleとmessageという属性を用意 */}
+  <React.StrictMode>
+    <App color = 'green' title = 'TOKYO VERDY!' message = 'これはグリーンを適用した際の表示です' />
   </React.StrictMode>
 )
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+// // Reactに用意されている仮想DOMを扱うため用意されている
+// // 引数に指定した「id=root」というエレメントをルートとするReactDOMオブジェクトを作成
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// // 仮想DOMの内容をレンダリングし、実際のDOMに反映
+// // 「厳格モードでAppコンポーネントをレンダリングして表示する」作業
+// root.render(
+//   // この<React.〜>の部分がJSX
+//   // React.StrictMode は「コードの実行をより厳格に制御するためのもの」
+//   <React.StrictMode> 
+//     <App title = "Hello,World!" message = "これはcolor=redの表示です。" color = "blue"/> 
+//     {/* /* Appタグにtitleとmessageという属性を用意 */}
+//   </React.StrictMode>
+// )
+
+
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
