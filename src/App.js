0fa784e1f920e424bd886ee78018579cf122586a
JUN SAITO <jun@JUNnoMacBook-puro.local>
2025-05-11 22:34:52 +0900

    赤い円の描画

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import './App.css'
 import { useState, useEffect } from 'react'
 
 function App(props){
-  var [flag, setFlag] = useState(true)
-  const doClick = (event) => {
-    setFlag(!flag)
-  }
+  var [count, setCount] = useState(0)
+  var [data, setData] = useState([
+    {
+      position: "absolute",
+      left: "0px", top: "0px",
+      width:"100%", height: "100%",
+      backgroundColor: "fff0",
+    }
+  ])
 
+  const doClick = (event)=> {
+    const ob = {
+            position: "absolute",
+      left: (event.pageX - 50) + "px",
+      top: (event.pageY - 50) + "px",
+      width:"100px", height: "100px",
+      backgroundColor: "#ff000066",
+      borderRadius: "50%",
+    }
+    data.push(ob)
+    setCount(count + 1)
+  }
   return (
     <div className='App'>
-      <h1 className={flag ? "TitleA" : "TitleB"}>{props.title}</h1>
-      <p className={flag ? "ClassB" : "ClassA"}>{flag ? "HOME" : "AWAY"}</p>
-      <button className='button' onClick={doClick}>切り替え</button>
+      <h1>{props.title}</h1>
+      <p>{count} objects.</p>
+      <div onClick={doClick}>
+        {data.map((item,key) =>
+        {return(<div style={item} key={key}></div>)
+      })}
+      </div>
     </div>
   )
 }
